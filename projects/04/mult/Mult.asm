79437f0ed943ac85a35099b10625baf7c074950f
uu64 <yusuke.t.0808@gmail.com>
2022-05-07 18:21:33 +0900

    add: Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,29 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    @i
+    M=1
+    @sum
+    M=0
+(LOOP)
+    @i
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JGT
+    @R0
+    D=M
+    @sum
+    M=M+D
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @sum
+    D=M
+    @R2
+    M=D
+    @END
+    0;JMP
