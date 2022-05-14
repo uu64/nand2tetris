d21937254b37ee79ad96a93e659f570d88e8e97a
uu64 <yusuke.t.0808@gmail.com>
2022-05-14 19:19:27 +0900

    refactoring

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -14,117 +14,78 @@
 // Put your code here.
 
 (MAIN)
-@24576
-D=M
-@CLEAR
-D;JEQ
-@FILL
-0;JMP
+    @i
+    M=0
+    @offset
+    M=0
 
+    // check key pressed or not
+    @24576
+    D=M
+    @key_pressed
+    M=D
+(VLOOP_START)
+    @i
+    D=M
+    @256
+    D=D-A
+    @VLOOP_END
+    D;JGE
+
+    @j
+    M=0
+(HLOOP_START)
+    @j
+    D=M
+    @32
+    D=D-A
+    @HLOOP_END
+    D;JGE
+
+    // clear or fill
+    @key_pressed
+    D=M
+    @CLEAR
+    D;JEQ
+    @FILL
+    0;JMP
 (CLEAR)
-// clear screen
-    @i
-    M=0
-    @offset
-    M=0
-(CLEAR_VLOOP_START)
-    @i
-    D=M
-    @256
-    D=D-A
-    @CLEAR_VLOOP_END
-    D;JGE
-
-    @j
-    M=0
-(CLEAR_HLOOP_START)
-    @j
-    D=M
-    @32
-    D=D-A
-    @CLEAR_HLOOP_END
-    D;JGE
-
-    // calculate address
     @offset
     D=M
     @SCREEN
     A=A+D
-
-    // clear
+    // reset
     M=0
-
-    // add offset
-    @offset
-    M=M+1
-    // add loop variable 
-    @j
-    M=M+1
-    @CLEAR_HLOOP_START
+    @CONTINUE
     0;JMP
-(CLEAR_HLOOP_END)
-    // add loop variable 
-    @i
-    M=M+1
-
-    // go to CLEAR_VLOOP_START
-    @CLEAR_VLOOP_START
-    D;JMP
-(CLEAR_VLOOP_END)
-    // go to MAIN
-    @MAIN
-    0;JMP
-
 (FILL)
-// fill screen
-    @i
-    M=0
-    @offset
-    M=0
-(FILL_VLOOP_START)
-    @i
-    D=M
-    @256
-    D=D-A
-    @FILL_VLOOP_END
-    D;JGE
-
-    @j
-    M=0
-(FILL_HLOOP_START)
-    @j
-    D=M
-    @32
-    D=D-A
-    @FILL_HLOOP_END
-    D;JGE
-
-    // calculate address
     @offset
     D=M
     @SCREEN
     A=A+D
-
     // fill
     M=-1
+    @CONTINUE
+    0;JMP
 
+(CONTINUE)
     // add offset
     @offset
     M=M+1
     // add loop variable 
     @j
     M=M+1
-    @FILL_HLOOP_START
+    @HLOOP_START
     0;JMP
-(FILL_HLOOP_END)
+(HLOOP_END)
     // add loop variable 
     @i
     M=M+1
 
-    // go to FILL_VLOOP_START
-    @FILL_VLOOP_START
+    // go to VLOOP_START
+    @VLOOP_START
     D;JMP
-(FILL_VLOOP_END)
+(VLOOP_END)
     // go to MAIN
     @MAIN
     0;JMP
