251087cea491d161a7bd3b9b8e8a6b44ba3ff0fb
uu64 <yusuke.t.0808@gmail.com>
2022-09-10 10:51:44 +0900

    add: test

--- vm/test.sh
+++ vm/test.sh
@@ -1,4 +1,12 @@
 #!/bin/sh -eu
 
-go run ./main.go ../projects/07/StackArithmetic/SimpleAdd/SimpleAdd.vm
-../tools/CPUEmulator.sh ../projects/07/StackArithmetic/SimpleAdd/SimpleAdd.tst
+test() {
+    echo "${1}/${2}"
+    go run ./main.go "../projects/07/${1}/${2}/${2}.vm"
+    ../tools/CPUEmulator.sh "../projects/07/${1}/${2}/${2}.tst"
+    echo 
+}
+
+test "StackArithmetic" "SimpleAdd"
+
+test "StackArithmetic" "StackTest"
